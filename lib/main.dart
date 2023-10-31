536aaae014c6c1fc9ad777b01503d3d587c5af96
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-31 13:13:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo/homepage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/cubit/project_cubit.dart';
+import 'package:todo/project_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ProjectCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'ToDo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+            centerTitle: true,
+          ),
+        ),
+        home: const ProjectScreen(),
       ),
-      home: const Homepage(),
     );
   }
 }
