9724aa57009d44b7b0d4e4a4595d5407fb2643b5
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-31 13:13:22 +0300

    Create homepage_screen.dart

---
+++ lib/presentation/screens/homepage_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('HomeScreen'),
+    );
+  }
+}
