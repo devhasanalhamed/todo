358988dba17b7639b96c40c218a3bbfbae146a51
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-31 13:13:28 +0300

    Delete todo_cubit.dart

--- lib/todo_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo/todo_state.dart';
-
-class TodoCubit extends Cubit<TodoState> {
-  TodoCubit() : super(InitialTodoState());
-
-  String name = 'Hello';
-}
