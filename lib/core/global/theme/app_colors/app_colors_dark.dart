0ece995c3b1d28c732eb7b5c23b3b48e7e5dd492
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-01 19:02:39 +0300

    Update app_colors_dark.dart

---
+++ lib/core/global/theme/app_colors/app_colors_dark.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColorsDark {
+  static const primaryColor = Color(0xFF333739);
+
+  static const secondaryColor = Color(0xFFFEFEFE);
+
+  static const scaffoldBackgroundColor = secondaryColor;
+
+  static const appBarBackgroundColor = primaryColor;
+
+  static const appBarForegroundColor = secondaryColor;
+
+  static const iconColor = primaryColor;
+
+  static const floatingActionButtonBackgroundColor = primaryColor;
+
+  static const cardBackgroundColor = primaryColor;
+}
