f5022617bd8a6db89ad03a86f9b1fcc8eda79645
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-01 12:43:34 +0300

    Update project_screen.dart

--- lib/project_screen.dart
+++ lib/project_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/cubit/project_cubit.dart';
 import 'package:todo/cubit/project_state.dart';
+import 'package:todo/presentation/widgets/search_card.dart';
 
 class ProjectScreen extends StatelessWidget {
   const ProjectScreen({Key? key}) : super(key: key);
@@ -13,13 +14,50 @@ class ProjectScreen extends StatelessWidget {
         builder: (context, state) {
           var cubit = ProjectCubit.get(context);
           return Scaffold(
-              appBar: AppBar(),
-              body: Center(
-                child: TextButton(
-                  onPressed: () => cubit.createDatabase(),
-                  child: const Text('Create Database'),
-                ),
-              ));
+            appBar: AppBar(
+              title: const Text('Todo'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SearchCard(),
+                  TextButton(
+                    onPressed: () => cubit.insertToDatabase(
+                      title: 'You Are Awesome',
+                      description: 'with patience every dream comes true',
+                      date: '11/11',
+                      time: '11:11',
+                    ),
+                    child: const Text('INSERT INTO TABLE'),
+                  ),
+                  TextButton(
+                    onPressed: () => cubit.readFromDatabase(),
+                    child: const Text('READ FROM TABLE'),
+                  ),
+                  TextButton(
+                    onPressed: () => cubit.updateIntoDatabase(
+                      id: 1,
+                      title: 'You Are On Fire 🔥',
+                      description:
+                          'You proved that success is a matter of time',
+                      date: '11-11-2023',
+                      time: '11:11',
+                    ),
+                    child: const Text('UPDATE INTO DATABASE'),
+                  ),
+                  TextButton(
+                    onPressed: () => cubit.deleteFromDatabase(id: 1),
+                    child: const Text('DELETE FROM DATABASE'),
+                  ),
+                  TextButton(
+                    onPressed: () => cubit.deleteDB(),
+                    child: const Text('DELETE DATABASE'),
+                  ),
+                ],
+              ),
+            ),
+          );
         });
   }
 }
