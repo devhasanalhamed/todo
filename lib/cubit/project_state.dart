b6e57a2d54c33f8ed53c54dbe2c02410566a9443
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-01 12:43:27 +0300

    Update project_state.dart

--- lib/cubit/project_state.dart
+++ lib/cubit/project_state.dart
@@ -3,3 +3,11 @@ abstract class ProjectState {}
 class InitialProjectState extends ProjectState {}
 
 class CreateDatabase extends ProjectState {}
+
+class InsertToDatabase extends ProjectState {}
+
+class ReadFromDatabase extends ProjectState {}
+
+class UpdateIntoDatabase extends ProjectState {}
+
+class DeleteFromDatabase extends ProjectState {}
