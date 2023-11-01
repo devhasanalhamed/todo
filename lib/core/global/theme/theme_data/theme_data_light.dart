d9741aa0daf4173487d177507e3956afad84d671
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-01 19:02:46 +0300

    Update theme_data_light.dart

--- lib/core/global/theme/theme_data/theme_data_light.dart
+++ lib/core/global/theme/theme_data/theme_data_light.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo/core/global/theme/app_colors/app_colors_light.dart';
 
 ThemeData get getThemeDataLight => ThemeData(
       useMaterial3: true,
+      // primarySwatch: AppColorsLight.primaryColor,
       colorScheme: const ColorScheme.light(
         primary: AppColorsLight.primaryColor,
       ),
+      //SCAFFOLD THEME
       scaffoldBackgroundColor: AppColorsLight.scaffoldBackgroundColor,
+      //APP BAR THEME
       appBarTheme: const AppBarTheme(
         foregroundColor: AppColorsLight.appBarForegroundColor,
         backgroundColor: AppColorsLight.appBarBackgroundColor,
+        elevation: 0,
         centerTitle: true,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Color.fromRGBO(33, 33, 33, 1),
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.light,
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
       ),
-      iconTheme: const IconThemeData(),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(),
-      textTheme: const TextTheme(),
+      //ICON THEME
+      iconTheme: const IconThemeData(
+        color: Colors.deepOrange,
+      ),
+      //FLOATING ACTION BUTTON THEME
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.deepOrange,
+      ),
+      //TEXT THEME
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w600,
+          color: Colors.deepPurple,
+        ),
+      ),
+      //CARD THEME
       cardTheme: const CardTheme(
         color: AppColorsLight.cardBackgroundColor,
+        elevation: 15.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusDirectional.all(
+            Radius.circular(5.0),
+          ),
+        ),
       ),
+      //
     );
