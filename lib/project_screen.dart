dd5e678f6f8a4248a22c6712a7ab98781475aa2e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-04 19:50:12 +0300

    Update project_screen.dart

--- lib/project_screen.dart
+++ lib/project_screen.dart
@@ -15,13 +15,13 @@ class ProjectScreen extends StatelessWidget {
     return BlocConsumer<ProjectCubit, ProjectState>(
       listener: (context, state) {
         log('cubit listener: something has been changed!');
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(state.message),
-            duration: const Duration(days: 1),
-          ),
-        );
+        // ScaffoldMessenger.of(context).clearSnackBars();
+        // ScaffoldMessenger.of(context).showSnackBar(
+        //   SnackBar(
+        //     content: Text(state.message),
+        //     duration: const Duration(days: 1),
+        //   ),
+        // );
       },
       builder: (context, state) {
         var cubit = ProjectCubit.get(context);
@@ -29,44 +29,50 @@ class ProjectScreen extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Todo'),
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SearchCard(),
-                TextButton(
-                  onPressed: () => cubit.insertToDatabase(
-                    title: 'You Are Awesome',
-                    description: 'with patience every dream comes true',
-                    date: '11/11',
-                    time: '11:11',
+          body: Column(
+            children: [
+              const SearchCard(),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cubit.tasks.length,
+                  itemBuilder: (context, index) => Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'title',
+                              style: TextStyle(
+                                fontSize: 22.0,
+                              ),
+                            ),
+                            const Text(
+                              'time',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.delete),
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'desc',
+                          style: TextStyle(
+                            fontSize: 26.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  child: const Text('INSERT INTO TABLE'),
                 ),
-                TextButton(
-                  onPressed: () => cubit.readFromDatabase(),
-                  child: const Text('READ FROM TABLE'),
-                ),
-                TextButton(
-                  onPressed: () => cubit.updateIntoDatabase(
-                    id: 1,
-                    title: 'You Are On Fire 🔥',
-                    description: 'You proved that success is a matter of time',
-                    date: '11-11-2023',
-                    time: '11:11',
-                  ),
-                  child: const Text('UPDATE INTO DATABASE'),
-                ),
-                TextButton(
-                  onPressed: () => cubit.deleteFromDatabase(id: 1),
-                  child: const Text('DELETE FROM DATABASE'),
-                ),
-                TextButton(
-                  onPressed: () => cubit.deleteDB(),
-                  child: const Text('DELETE DATABASE'),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
