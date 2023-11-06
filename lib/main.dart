833e787364890960d317ce30fecefdfa723c9e89
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-06 12:59:57 +0300

    Update main.dart
    add animated splash screen, easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:todo/core/global/theme/theme_data/theme_data_light.dart';
 import 'package:todo/cubit/project_cubit.dart';
 import 'package:todo/project_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      path: 'assets/translations',
+      supportedLocales: const [Locale('ar', 'SA'), Locale('en', 'US')],
+      fallbackLocale: const Locale('en', 'US'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +35,23 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'ToDo',
         theme: getThemeDataLight,
-        home: const ProjectScreen(),
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        home: AnimatedSplashScreen(
+          splash: Image.asset(
+            'assets/images/app_logo.png',
+            width: 300,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          nextScreen: const ProjectScreen(),
+          backgroundColor: Colors.black,
+          duration: 2000,
+          centered: true,
+          pageTransitionType: PageTransitionType.bottomToTop,
+          splashIconSize: 300,
+        ),
       ),
     );
   }
