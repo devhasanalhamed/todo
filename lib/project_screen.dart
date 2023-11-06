aea0f183dbf8461b109e99a3ccc57d8329a69305
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-06 06:42:41 +0300

    Update project_screen.dart
    add the user interface for loading and a conditional widget

--- lib/project_screen.dart
+++ lib/project_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/cubit/project_cubit.dart';
@@ -12,82 +13,91 @@ class ProjectScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ProjectCubit, ProjectState>(
-      listener: (context, state) {
-        log('cubit listener: something has been changed!');
-        // ScaffoldMessenger.of(context).clearSnackBars();
-        // ScaffoldMessenger.of(context).showSnackBar(
-        //   SnackBar(
-        //     content: Text(state.message),
-        //     duration: const Duration(days: 1),
-        //   ),
-        // );
-      },
-      builder: (context, state) {
-        var cubit = ProjectCubit.get(context);
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Todo'),
-          ),
-          body: Column(
-            children: [
-              const SearchCard(),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: cubit.tasks.length,
-                  itemBuilder: (context, index) => Card(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddTasksScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: BlocConsumer<ProjectCubit, ProjectState>(
+        listener: (context, state) {
+          log('cubit listener: something has been changed!');
+        },
+        builder: (context, state) {
+          var cubit = ProjectCubit.get(context);
+          return ConditionalBuilder(
+            condition: state is! SuccessReadDataFromDatabase,
+            fallback: (context) =>
+                const Center(child: CircularProgressIndicator()),
+            builder: (context) => cubit.tasks.isEmpty
+                ? const Center(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              cubit.tasks[index]['title'],
-                              style: const TextStyle(
-                                fontSize: 22.0,
-                              ),
-                            ),
-                            Text(
-                              cubit.tasks[index]['time'],
-                              style: const TextStyle(
-                                fontSize: 16.0,
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () => cubit.deleteFromDatabase(
-                                  id: cubit.tasks[index]['id']),
-                              icon: const Icon(Icons.delete),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          cubit.tasks[index]['description'],
-                          style: const TextStyle(
-                            fontSize: 26.0,
-                          ),
-                        ),
+                        Icon(Icons.hourglass_empty),
+                        Text('No Tasks Yet'),
                       ],
                     ),
+                  )
+                : Column(
+                    children: [
+                      const SearchCard(),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: cubit.tasks.length,
+                          itemBuilder: (context, index) => Card(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      cubit.tasks[index]['title'],
+                                      style: const TextStyle(
+                                        fontSize: 22.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      cubit.tasks[index]['time'],
+                                      style: const TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    IconButton(
+                                      onPressed: () => cubit.deleteFromDatabase(
+                                          id: cubit.tasks[index]['id']),
+                                      icon: const Icon(Icons.delete),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  cubit.tasks[index]['description'],
+                                  style: const TextStyle(
+                                    fontSize: 26.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AddTasksScreen(),
-                ),
-              );
-            },
-            child: const Icon(Icons.add),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
