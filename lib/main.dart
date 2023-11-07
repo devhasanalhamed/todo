53dfbc8e3bd43d9c28851a6780817f1ed919335e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-07 11:14:59 +0300

    Update main.dart
    provide an approach to fetch user preferences theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,31 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo/core/global/theme/theme_data/theme_data_dark.dart';
 import 'package:todo/core/global/theme/theme_data/theme_data_light.dart';
 import 'package:todo/cubit/project_cubit.dart';
+import 'package:todo/cubit/project_state.dart';
 import 'package:todo/project_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  final prefBool = pref.getBool('prefBool');
   runApp(
     EasyLocalization(
       path: 'assets/translations',
       supportedLocales: const [Locale('ar', 'SA'), Locale('en', 'US')],
       fallbackLocale: const Locale('en', 'US'),
-      child: const MyApp(),
+      child: MyApp(prefBool: prefBool),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool? prefBool;
+  const MyApp({super.key, required this.prefBool});
 
   // This widget is the root of your application.
   @override
@@ -29,29 +35,38 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => ProjectCubit()..createDatabase(),
+          create: (context) => ProjectCubit()
+            ..createDatabase()
+            ..changeThemeMode(prefBool),
         ),
       ],
-      child: MaterialApp(
-        title: 'appTitle'.tr(),
-        theme: getThemeDataLight,
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        home: AnimatedSplashScreen(
-          splash: Image.asset(
-            'assets/images/app_logo.png',
-            width: 300,
-            height: 300,
-            fit: BoxFit.cover,
-          ),
-          nextScreen: const ProjectScreen(),
-          backgroundColor: Colors.black,
-          duration: 2000,
-          centered: true,
-          pageTransitionType: PageTransitionType.bottomToTop,
-          splashIconSize: 300,
-        ),
+      child: BlocBuilder<ProjectCubit, ProjectState>(
+        builder: (context, state) {
+          var cubit = ProjectCubit.get(context);
+          return MaterialApp(
+            title: 'appTitle'.tr(),
+            theme: getThemeDataLight,
+            darkTheme: getThemeDataDark,
+            themeMode: cubit.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            home: AnimatedSplashScreen(
+              splash: Image.asset(
+                'assets/images/app_logo.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              nextScreen: const ProjectScreen(),
+              backgroundColor: Colors.black,
+              duration: 2000,
+              centered: true,
+              pageTransitionType: PageTransitionType.bottomToTop,
+              splashIconSize: 300,
+            ),
+          );
+        },
       ),
     );
   }
