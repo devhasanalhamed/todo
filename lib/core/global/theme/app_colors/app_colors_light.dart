01e010fd10312e20455887d0761b2f2d7dc612b6
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-08 13:25:43 +0300

    Update app_colors_light.dart
    add extra colors to the app

--- lib/core/global/theme/app_colors/app_colors_light.dart
+++ lib/core/global/theme/app_colors/app_colors_light.dart
@@ -5,15 +5,23 @@ class AppColorsLight {
 
   static const secondaryColor = Color(0xFFFEFEFE);
 
+  static const thirdColor = Color(0xFFFF5722);
+
+  static const primaryTextColor = Color(0xFFFFFFFF);
+
+  static const secondaryTextColor = Color(0xFFFFFFFF);
+
+  static const iconColor = primaryColor;
+
+  static const statusBarColor = primaryColor;
+
   static const scaffoldBackgroundColor = secondaryColor;
 
   static const appBarBackgroundColor = primaryColor;
 
   static const appBarForegroundColor = secondaryColor;
 
-  static const iconColor = primaryColor;
-
   static const floatingActionButtonBackgroundColor = primaryColor;
 
-  static const cardBackgroundColor = primaryColor;
+  static const cardBackgroundColor = thirdColor;
 }
